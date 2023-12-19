4039599157fb517fab374ad5bad6ee60b7241a7e
Vishnu Rajeevan <github@vishnu.email>
2023-12-18 20:22:21 -0500

    fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mkdir -p run
 mkdir -p data
-server/build/install/remnant-discord-bot/bin/remnant-discord-bot run \
-    --token "${DISCORD_TOKEN}"
+bin/remnant-discord-bot run --token "${DISCORD_TOKEN}"
